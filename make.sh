e95df0b7ba9ad584d2ef8b04ddd33a4729ca848e
thblt <thibault@thb.lt>
2015-10-17 16:29:46 +0200

    Make.sh peut ne générer qu'un sous-ensemble des fiches

--- make.sh
+++ make.sh
@@ -1,4 +1,16 @@
-ls fiches | grep "^[[:digit:]]" | xargs -I X echo "\input{fiches/X}" > fiches/_everything.tex
+#!/bin/sh
+
+out="fiches/_everything.tex"
+
+if [ -z $1 ]; then
+    ls fiches/[0-9]*.tex | xargs -I X echo "\input{X}" > $out
+else
+    rm $out
+    for i in $@; do
+        ls fiches/`printf %03d $i`*.tex | xargs -I X echo "\input{X}" >> $out
+    done
+fi
+
 if xelatex --shell-escape master
     then
     if biber master
