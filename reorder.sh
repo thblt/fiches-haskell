b820db57c031a1b8edc698a3cc7e9487af41eafd
thblt <thibault@thb.lt>
2015-10-27 15:41:23 +0100

    reorder.sh ne produit plus d'erreur qd un dossier est renommé

--- reorder.sh
+++ reorder.sh
@@ -17,10 +17,11 @@ fn=0 # File number
 for path in $root/[0-9]*; do
     pn=$(($pn+10))
     folder=`basename $path`
-    git_move $path $root/`printf %02d-%s ${pn} ${folder#*-}`
-    for file in $path/[0-9]*.tex;  do
+    newpath=$root/`printf %02d-%s ${pn} ${folder#*-}`
+    git_move $path $newpath
+    for file in $newpath/[0-9]*.tex;  do
         fn=$(($fn+10))
         filename=`basename $file`
-        git_move $file $path/`printf %03d-%s ${fn} ${filename#*-}`
+        git_move $file $newpath/`printf %03d-%s ${fn} ${filename#*-}`
     done
 done
