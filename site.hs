8c79eae22cc2324ea9d95e7d81da26dff5863b58
thblt <thibault@thb.lt>
2016-12-21 23:57:42 +0100

    Commit all the things

--- site.hs
+++ site.hs
@@ -1,79 +1,70 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Default (def)
-import Control.Monad (liftM)
-import Data.Monoid (mappend, mconcat, mempty)
-import Text.Pandoc.Options
+import           Control.Monad                  (liftM)
+import           Data.Default                   (def)
+import           Data.Monoid                    (mappend, mconcat, mempty)
+import           Hakyll
+import           Hakyll.Core.Identifier.Pattern (Pattern (..))
+import           Hakyll.Web.Sass
+import           Text.Pandoc.Options
+import           Text.Pandoc
 
-import Hakyll
+bibFile, cslFile :: String
+bibFile = "bibliography.bib"
+cslFile = "theme/iso690-author-date-fr.csl"
 
 main :: IO ()
 main = hakyll $ do
-    match "contents/**.md" $
-      compile $ customCompiler
-        >>= return . fmap demoteHeaders
-        >>= return . fmap demoteHeaders
-        >>= loadAndApplyTemplate "theme/tpl/item.html" defaultContext
-        >>= relativizeUrls
 
-    create ["index.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- loadAll "contents/**.md"
-            let archiveCtx =
-                    listField "posts" defaultContext (return posts) `mappend`
-                    defaultContext
+  match (fromGlob bibFile) $ compile biblioCompiler
 
-            makeItem ""
-                >>= loadAndApplyTemplate "theme/tpl/cards.html" archiveCtx
-                >>= loadAndApplyTemplate "theme/tpl/default.html" archiveCtx
-                >>= relativizeUrls
+  match (fromGlob cslFile) $ compile cslCompiler
 
-    match "bibliography.bib" $ compile biblioCompiler
+  match "haskell.org" $ do
+    route $ setExtension "html"
+    compile $
+        bibtexCompiler cslFile bibFile >>=
+        loadAndApplyTemplate "theme/tpl/default.html" defaultContext >>=
+        relativizeUrls
 
-    match  "theme/iso690-author-date-fr.csl" $ compile cslCompiler
+  match "theme/scss/theme.scss" $ do
+      route   $ gsubRoute "theme/scss/" (const "theme/") `composeRoutes` setExtension "css"
+      compile $ sassCompilerWith def {
+        sassOutputStyle = SassStyleCompressed -- SassStyleCompact
+        }
 
-    match "contents_extras/**" $ do
-        route   $ gsubRoute "contents_extras/" (const "") `composeRoutes`
-            setExtension "html"
-        compile $ customCompiler
-            >>= return . fmap (demoteHeaders . demoteHeaders)
-            >>= loadAndApplyTemplate "theme/tpl/page.html" defaultContext
-            >>= loadAndApplyTemplate "theme/tpl/default.html" defaultContext
-            >>= relativizeUrls
-
-    match "theme/scss/theme.scss" $ do
-        route   $ gsubRoute "theme/scss/" (const "theme/") `composeRoutes` setExtension "css"
-        compile $ getResourceString
-            -- FIXME "-r gridle" works, but really feels dirty. There must be
-            -- a way for required scss libraries to be listed outside of here. A
-            -- _theme/scss/gems file would be better.
-            >>= withItemBody (unixFilter "sass" ["-I", "theme/scss", "-r", "gridle", "--scss", "--compass"])
-            >>= return . fmap compressCss
-            -- FIXME This doesn't create dependencies for included _*.scss
-            -- files. See here://http://www.blaenkdenum.com/posts/extra-dependencies-in-hakyll/
-
-    match ("theme/img/*" .||. "theme/js/*") $ do
+  match ("theme/img/*" .||. "theme/js/*") $ do
         route idRoute
         compile copyFileCompiler
 
-    match "theme/tpl/*" $ compile templateCompiler
+  match "theme/tpl/*" $ compile templateCompiler
 
-customCompiler = 
-  getResourceBody
-  >>= (withItemBody $ unixFilter "gpp" ["-T", "--include", "html.gpp"])
-  >>= renderPandocWith defaultHakyllReaderOptions customPandocWriterOptions
 
-customPandocWriterOptions :: WriterOptions
-customPandocWriterOptions = defaultHakyllWriterOptions {
-  writerEmailObfuscation = NoObfuscation    
-}
+bibtexCompiler :: String -> String -> Compiler (Item String)
+bibtexCompiler cslFileName bibFileName = do
+    csl <- load $ fromFilePath cslFileName
+    bib <- load $ fromFilePath bibFileName
+    liftM (writePandocWith myWriterOptions)
+      (getResourceBody >>= readPandocBiblio myReaderOptions csl bib)
+      where
 
--- bibtexCompiler = do 
+        myReaderOptions :: ReaderOptions
+        myReaderOptions = def {
+          readerParseRaw = True,
+          readerSmart = True }
+
+        myWriterOptions :: WriterOptions
+        myWriterOptions = def { writerSectionDivs = True,
+                                writerHtml5 = True,
+                                writerHighlight = True,
+                                writerHTMLMathMethod = Text.Pandoc.MathML Nothing,
+                                writerEmailObfuscation = NoObfuscation }
+
+-- bibtexCompiler = do
 --   csl <- load "theme/iso690-author-date-fr.csl"
 --   bib <- load "bibliography.bib"
 --   getResourceBody
 --   >>= readPandocBiblio def csl bib
 --   >>= return . writePandoc
--- 
+--
---
+++ transform.sh
@@ -0,0 +1,2 @@
+cp $1 $1.old
+gpp -T +n --include ~/Documents/Code/haskell.thb.lt/orgmode.gpp $1.old > $1
